28141355d4662d13ddeeaba640bea1490459d255
jack.savory <jack.savory@thomsonreuters.com>
2017-10-20 19:32:57 +0100

    Initial commit

---
+++ Authenticator/src/main/scala/com/jack/Authenticator.scala
@@ -0,0 +1,9 @@
+package com.jack
+
+object Authenticator {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
---
+++ Trading/src/main/scala/com/jack/Trading.scala
@@ -0,0 +1,9 @@
+package main.scala.com.jack
+
+object Trading {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
